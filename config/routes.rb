db46c3afb304cadf46b858ce1fde350be7b6fe4f
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-19 17:14:37 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,7 @@ Rails.application.routes.draw do
 
   resources :users_admin, :controller => 'users', only: [:index, :create]
 
-  resources :divisions, only: [:index, :create, :update, :destroy, :edit] do
-  	resources :categories, only: [:edit, :create, :destroy]
-  end
-
-  resources :categories do
-    resources :company_skills, only: [:create, :update, :destroy]
-  end
-
   # divisions
   post "division_update_api", to: "divisions#update_api"
 
-
-  # categories
-  post "category_new_api", to: "categories#new_api"
-  post "category_update_api", to: "categories#update_api"
-  post "category_destroy_api", to: "categories#destroy_api"
-  # company_skills
-  post "company_skill_new_api", to: "company_skills#new_api"
-  post "company_skill_update_api", to: "company_skills#update_api"
-  post "company_skill_destroy_api", to: "company_skills#destroy_api"
 end
